1d8267f93d9f59e34c19daa70491ed4e96c64351
Jeff <67961143+Wheeles7@users.noreply.github.com>
2020-08-20 16:10:07 -0500

    Color Palette finished

---
+++ Color Palette/buttons.js
@@ -0,0 +1,30 @@
+var el_up = document.getElementById("GFG_UP"); 
+
+function changeColor(color) { 
+    document.body.style.background = color; 
+} 
+  
+function gfg_Run() { 
+    changeColor('green'); 
+}      
+function gfg_Run1() { 
+    changeColor('yellow'); 
+}      
+function gfg_Run2() { 
+    changeColor('orange'); 
+}      
+function gfg_Run3() { 
+    changeColor('purple'); 
+}      
+function gfg_Run4() { 
+    changeColor('teal'); 
+}      
+function gfg_Run5() { 
+    changeColor('red'); 
+}      
+function gfg_Run6() { 
+    changeColor('darkblue'); 
+}      
+function gfg_Run7() { 
+    changeColor('gray'); 
+}  
